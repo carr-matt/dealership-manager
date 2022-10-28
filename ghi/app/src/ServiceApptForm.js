a560764853e889da93aadc87e1976600cfc20b41
Matt <carr.matthew.lee@gmail.com>
2022-10-28 10:28:32 -0500

    ServiceApptForm working again, ServiceApptsList appears to be

--- ghi/app/src/ServiceApptForm.js
+++ ghi/app/src/ServiceApptForm.js
@@ -1,141 +1,126 @@
-import React from "react";
+import React from 'react';
 
-class ServiceApptForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            vin: "",
-            owner: "",
-            date: "",
-            time: "",
-            tech: "",
-            techs: [],
-            reason: "",
-        }
 
-        this.handleVinChange = this.handleVinChange.bind(this)
-        this.handleOwnerChange = this.handleOwnerChange.bind(this)
-        this.handleDateChange = this.handleDateChange.bind(this)
-        this.handleTimeChange = this.handleTimeChange.bind(this)
-        this.handleTechChange = this.handleTechChange.bind(this)
-        this.handleReasonChange = this.handleReasonChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-
+class ServiceForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      owner_name: "",
+      appointment: "",
+      tech_name: "",
+      reason: "",
+      vin: "",
+      tech_names: [],
     }
+  }
 
-    handleVinChange(event) {
-        const value = event.target.value
-        this.setState({vin: value})
+  async componentDidMount() {
+    const url = 'http://localhost:8080/api/tech/';
+
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      this.setState({ tech_names: data.technicians })
     }
-    handleOwnerChange(event) {
-        const value = event.target.value
-        this.setState({owner: value})
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { ...this.state };
+    delete data.tech_names;
+    const serviceUrl = 'http://localhost:8080/api/service/';
+    const fetchOptions = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+
+    const serviceResponse = await fetch(serviceUrl, fetchOptions);
+
+    if (serviceResponse.ok) {
+
+      const newService = await serviceResponse.json();
+      console.log(newService)
+      const cleared = {
+        owner_name: "",
+        appointment: "",
+        tech_name: "",
+        reason: "",
+        vin: "",
+      }
+      this.setState(cleared);
     }
-    handleDateChange(event) {
-        const value = event.target.value
-        this.setState({date: value})
-    }
-    handleTimeChange(event) {
-        const value = event.target.value
-        this.setState({time: value})
-    }
-    handleTechChange(event) {
-        const value = event.target.value
-        this.setState({tech: value})
-    }
-    handleReasonChange(event) {
-        const value = event.target.value
-        this.setState({reason: value})
-    }
+  }
 
-    async handleSubmit(event) {
-        event.preventDefault()
-        const data = {...this.state}
-        delete data.techs
-        console.log(data)
-        const apptUrl = 'http://localhost:8080/api/service/appt/'
-        const fetchConfig = {
-            method: "post",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
+  handleOwnerNameChange = (event) => {
+    const value = event.target.value;
+    this.setState({ owner_name: value })
+  }
+  handleAppointmentChange = (event) => {
+    const value = event.target.value;
+    this.setState({ appointment: value })
+  }
+  handleReasonChange = (event) => {
+    const value = event.target.value;
+    this.setState({ reason: value })
+  }
+  handleVinNumberChange = (event) => {
+    const value = event.target.value;
+    this.setState({ vin: value })
+  }
+  handleTechnicianChange = (event) => {
+    const value = event.target.value;
+    this.setState({ tech_name: value })
+  }
 
-        const response = await fetch(apptUrl, fetchConfig)
 
-        if (response.ok) {
-            const Appt = await response.json()
-            console.log(Appt)
-            const cleared = {
-                vin: "",
-                owner: "",
-                date: "",
-                time: "",
-                tech: "",
-                reason: "",
-            }
-            this.setState(cleared)
-        }
-        }
-
-    async componentDidMount() {
-        const url = 'http://localhost:8080/api/service/tech/'
-
-        const response = await fetch(url)
-
-        if (response.ok) {
-            const data = await response.json()
-            console.log(data)
-            this.setState({techs: data.tech })
-        }
-    }
-
-render() {
+  render() {
     return (
-<div className="row">
+      <div className="row">
         <div className="offset-3 col-6">
-            <div className="shadow p-4 mt-4">
-            <h1>Schedule an Service Appointment</h1>
+          <div className="shadow p-4 mt-4">
+            <h1>Schedule an Appointment</h1>
             <form onSubmit={this.handleSubmit} id="create-appt-form">
-                <div className="form-floating mb-3">
-                <input value={this.state.vin} onChange={this.handleVinChange} placeholder="vin" required type="text" name="vin" id="vin" className="form-control"/>
-                <label htmlFor="vin">VIN</label>
-                </div>
-                <div className="form-floating mb-3">
-                <input value={this.state.owner} onChange={this.handleOwnerChange} placeholder="owner" name="owner" type="text" id="owner" className="form-control"/>
-                <label htmlFor="owner">Owner</label>
-                </div>
-                <div className="form-floating mb-3">
-                <input value={this.state.date} onChange={this.handleDateChange} placeholder="date" name="date" type="date" id="date" className="form-control"/>
-                <label htmlFor="date">Date</label>
-                </div>
-                <div className="mb-3">
-                <div className="form-floating mb-3">
-                <input value={this.state.time} onChange={this.handleTimeChange} placeholder="time" name="time" type="time" id="time" className="form-control"/>
-                <label htmlFor="time">Time</label>
-                </div>
-                <div className="form-floating mb-3">
-                <input value={this.state.reason} onChange={this.handleReasonChange} placeholder="reason" name="color" type="text" id="reason" className="form-control"/>
-                <label htmlFor="reason">Reason for Service</label>
-                </div>
-                <select value={this.state.tech} onChange={this.handleTechChange} required id="tech" name="tech" className="form-select">
-                    <option value="">Choose a Tech</option>
-                    {this.state.techs.map(tech => {
+              <div className="form-floating mb-3">
+                <input onChange={this.handleOwnerNameChange} value={this.state.owner_name} placeholder="Owner Name" required type="text" name="owner_name" id="owner_name"
+                  className="form-control" />
+                <label htmlFor="name">Owner Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input onChange={this.handleAppointmentChange} value={this.state.appointment} placeholder="Name" required type="datetime-local" name="appointment" id="appointment"
+                  className="form-control" />
+                <label htmlFor="name">Appointment</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input onChange={this.handleReasonChange} value={this.state.reason} placeholder="Name" required type="text" name="reason" id="reason"
+                  className="form-control" />
+                <label htmlFor="name">Reason</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input onChange={this.handleVinNumberChange} value={this.state.vin} placeholder="Name" required type="text" name="vin" id="vin"
+                  className="form-control" />
+                <label htmlFor="name">VIN</label>
+              </div>
+              <div className="mb-3">
+                <select onChange={this.handleTechnicianChange} value={this.state.tech_name} required name="tech_name" id="tech_name" className="form-select">
+                  <option value="">Technician</option>
+                  {this.state.tech_names.map(tech_name => {
                     return (
-                        <option key={tech.id} value={tech.id}>{tech.name}
-                        </option>
-                    )
-                    })}
+                      <option key={tech_name.id} value={tech_name.tech_name}> {tech_name.tech_name} </option>
+                    );
+                  })}
                 </select>
-                </div>
-                <button className="btn btn-primary">Create</button>
+              </div>
+              <button className="btn btn-primary">Submit</button>
             </form>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     )
-    }
+  }
 }
 
-export default ServiceApptForm
+
+export default ServiceForm
--- ghi/app/src/ServiceApptsList.js
+++ ghi/app/src/ServiceApptsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Outlet from "react-router-dom";
+
 
 
 class ServiceApptsList extends React.Component {
@@ -11,7 +11,7 @@ class ServiceApptsList extends React.Component {
   }
 
   async componentDidMount() {
-    const servicesUrl = 'http://localhost:8080/api/service/appt';
+    const servicesUrl = 'http://localhost:8080/api/service/';
     const response = await fetch(servicesUrl);
     if (response.ok) {
       const data = await response.json();
@@ -21,7 +21,7 @@ class ServiceApptsList extends React.Component {
 
   handleOnRemove = async (event) => {
     const id = event.target.value;
-    const serviceUrl = `http://localhost:8080/api/service/appt/${id}/`;
+    const serviceUrl = `http://localhost:8080/api/service/${id}/`;
     const fetchConfig = {
       method: "DELETE",
       headers: {
@@ -31,7 +31,7 @@ class ServiceApptsList extends React.Component {
     const response = await fetch(serviceUrl, fetchConfig);
     if (response.ok) {
       this.setState({
-        services: this.state.services.filter(service => service.id != id)
+        services: this.state.services.filter(service => service.id !== id)
       })
     }
   }
@@ -42,7 +42,7 @@ class ServiceApptsList extends React.Component {
     delete data.services;
 
     const id = event.target.value;
-    const serviceUrl = `http://localhost:8080/api/service/appt/${id}/`;
+    const serviceUrl = `http://localhost:8080/api/service/${id}/`;
     const fetchConfig = {
       method: "PUT",
       body: JSON.stringify({ finished: true }),
@@ -50,11 +50,12 @@ class ServiceApptsList extends React.Component {
         'Content-Type': 'application/json'
       }
     };
+
     const response = await fetch(serviceUrl, fetchConfig);
 
     if (response.ok) {
       this.setState({
-        services: this.state.services.filter(service => service.id != id)
+        services: this.state.services.filter(service => service.id !== id)
       })
     }
 
@@ -66,7 +67,7 @@ class ServiceApptsList extends React.Component {
     return (
       <>
         <div className="my-5 container">
-          <table className="table table-success table-striped">
+          <table className="table table-striped">
             <thead>
               <tr>
                 <th>VIP</th>
@@ -76,7 +77,7 @@ class ServiceApptsList extends React.Component {
                 <th>Time</th>
                 <th>Technician</th>
                 <th>Reason</th>
-                <th>Cancel/Finish</th>
+                <th>Cancel/Complete</th>
               </tr>
             </thead>
 
@@ -84,25 +85,27 @@ class ServiceApptsList extends React.Component {
               {this.state.services.map(service => {
                 return !service.finished && (
                   <tr key={service.id}>
+
                     {service.vip ? <td>VIP</td> : <td>NOT VIP</td>}
+
                     <td>{service.vin}</td>
                     <td>{service.owner_name}</td>
                     <td>{new Date(service.appointment).toLocaleDateString()}</td>
                     <td>{new Date(service.appointment).toLocaleTimeString()}</td>
                     <td>{service.tech_name.tech_name}</td>
-                    <td>{service.service_name}</td>
+                    <td>{service.reason}</td>
                     <td>
-                      <button onClick={this.handleOnRemove} value={service.id} className="btn btn-danger btn-sm">CANCEL</button>
-                      <button onClick={this.handleFinished} value={service.id} className="btn btn-success btn-sm">FINISH</button>
+                      <button onClick={this.handleOnRemove} value={service.id} className="btn btn-sm">
+                        Cancel</button>
+                      <button onClick={this.handleFinished} value={service.id} className="btn btn-sm">
+                        Complete</button>
                     </td>
                   </tr>
                 );
               })}
             </tbody>
-
           </table>
         </div>
-        {/* <Outlet /> */}
       </>
     )
   }
